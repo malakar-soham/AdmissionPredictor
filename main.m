f2e54e537f1379303368fd0526ca8e9d5a35ad89
Soham Malakar <malakar_soham@outlook.com>
2019-07-22 23:36:24 +0530

    Update and rename ex2.m to main.m

--- main.m
+++ main.m
@@ -5,7 +5,7 @@ clear ; close all; clc
 %  The first two columns contains the exam scores and the third column
 %  contains the label.
 
-data = load('ex2data1.txt');
+data = load('data.txt');
 X = data(:, [1, 2]); y = data(:, 3);
 
 %% ==================== Part 1: Plotting ====================
